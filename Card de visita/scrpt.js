460c71ed21c94cba0ac1675d228e622b4177c009
bknhrt <161094056+bknhrt@users.noreply.github.com>
2026-05-31 23:05:12 -0300

    Delete Card de visita directory

--- Card de visita/scrpt.js
+++
@@ -1,9 +0,0 @@
-const gif = document.querySelector(".cursor-gif");
-
-document.addEventListener("mousemove", (e) => {
-
-  gif.style.left = e.clientX + "px";
-
-  gif.style.top = e.clientY + "px";
-
-});
